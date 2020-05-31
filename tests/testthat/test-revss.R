4e8b107326458352f90e94188af3287c5fad3ad9
Avraham Adler <avraham.adler@gmail.com>
2020-05-31 13:40:02 -0400

    Ensure README is up-to-date with version too.

--- tests/testthat/test-revss.R
+++ tests/testthat/test-revss.R
@@ -118,5 +118,7 @@ context("Package Maintenance")
 test_that("Version", {
   expect_match(toBibtex(citation('revss')),
                as.character(packageVersion('revss')), fixed = TRUE, all = FALSE)
+  expect_match(scan("../../README.md", what = 'character'),
+               as.character(packageVersion('revss')), fixed = TRUE, all = FALSE)
 })
 
