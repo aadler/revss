64a26dce6df8c831d20d64cd431bb1c7935c7c14
Avraham Adler <avraham.adler@gmail.com>
2021-01-11 22:32:32 -0500

    Unused

--- tests/testthat/test-citation.R
+++ tests/testthat/test-citation.R
@@ -1,7 +1,5 @@
 test_that("Version", {
   expect_match(toBibtex(citation('revss')),
                as.character(packageVersion('revss')), fixed = TRUE, all = FALSE)
-  # expect_match(scan("../../README.md", what = 'character'),
-  #              as.character(packageVersion('revss')), fixed = TRUE, all = FALSE)
 })
 
