72b578b8f5fe49491c48a1d69a1004080394d335
Avraham Adler <avraham.adler@gmail.com>
2023-01-31 15:54:08 -0500

    Make more consistent with style guide

--- inst/tinytest/test_revss.R
+++ inst/tinytest/test_revss.R
@@ -1,3 +1,4 @@
+tol <- sqrt(.Machine$double.eps)
 ## Generate Test Data
 eff_seed <- sample(65536, 1)
 set.seed(eff_seed)
@@ -9,10 +10,10 @@ y <- c(9, 2, 14, 4)
 naErr <- "There are NAs in the data yet na.rm is FALSE"
 
 ## ADM Tests
-expect_equal(adm(x5), adm5 * sqrt(pi / 2))
-expect_equal(adm(c(x5, NA), na.rm = TRUE), adm5 * sqrt(pi / 2))
-expect_equal(adm(x5, constant = 1), adm5)
-expect_equal(adm(c(x5, NA), constant = 1, na.rm = TRUE), adm5)
+expect_equal(adm(x5), adm5 * sqrt(pi / 2), tolerance = tol)
+expect_equal(adm(c(x5, NA), na.rm = TRUE), adm5 * sqrt(pi / 2), tolerance = tol)
+expect_equal(adm(x5, constant = 1), adm5, tolerance = tol)
+expect_equal(adm(c(x5, NA), constant = 1, na.rm = TRUE), adm5, tolerance = tol)
 expect_true(is.na(adm(c(x5, NA))))
 expect_true(is.na(adm(c(x5, NA), constant = 1)))
 
@@ -48,29 +49,30 @@ robLocScaleTest <- function(x, scale, na.rm = FALSE,
   }
 }
 
-expect_equal(robLoc(x5), robLocTest(x5))
+expect_equal(robLoc(x5), robLocTest(x5), tolerance = tol)
 expect_equal(robLoc(x5, tol = .Machine$double.eps),
-             robLocTest(x5, .Machine$double.eps))
-expect_equal(robLoc(c(1, 9, 7)), median(c(1, 9, 7)))
+             robLocTest(x5, .Machine$double.eps), tolerance = tol)
+expect_equal(robLoc(c(1, 9, 7)), median(c(1, 9, 7)), tolerance = tol)
 
 # Known Location
-expect_equal(robLoc(y, scale = 5), robLocScaleTest(y, scale = 5))
+expect_equal(robLoc(y, scale = 5), robLocScaleTest(y, scale = 5),
+             tolerance = tol)
 expect_equal(robLoc(c(1, 8, 12), scale = 5),
-             robLocScaleTest(c(1, 8, 12), scale = 5))
-expect_equal(robLoc(c(1, 8), scale = 5), median(c(1, 8)))
+             robLocScaleTest(c(1, 8, 12), scale = 5), tolerance = tol)
+expect_equal(robLoc(c(1, 8), scale = 5), median(c(1, 8)), tolerance = tol)
 expect_false(isTRUE(all.equal(robLoc(c(1, 8, 12), scale = 5),
                               median(c(1, 8, 12)))))
 # RobLoc Error Trapping
 expect_error(robLoc(c(x5, NA)), pattern = naErr)
-expect_equal(robLoc(c(x5, NA), na.rm = TRUE), robLoc(x5))
+expect_equal(robLoc(c(x5, NA), na.rm = TRUE), robLoc(x5), tolerance = tol)
 
 ## RobScale Tests
-expect_equal(robScale(y), 5.88048107946198)
+expect_equal(robScale(y), 5.88048107946198, tolerance = tol)
 
 # Test Exception Handling
-expect_equal(robScale(y[1:3]), mad(y[1:3]))
-expect_equal(robScale(c(0.00001, 0, 4)), adm(c(0.00001, 0, 4)))
-expect_equal(robScale(c(0.0001, 0, 4)), mad(c(0.0001, 0, 4)))
+expect_equal(robScale(y[1:3]), mad(y[1:3]), tolerance = tol)
+expect_equal(robScale(c(0.00001, 0, 4)), adm(c(0.00001, 0, 4)), tolerance = tol)
+expect_equal(robScale(c(0.0001, 0, 4)), mad(c(0.0001, 0, 4)), tolerance = tol)
 # Excel precision probably lacking here.
 expect_equal(robScale(c(0.0001, 0, 0, 4)), 0.000101541283431, tolerance = 1e-7)
 
@@ -79,7 +81,7 @@ robScaleLocTest <- function(x, loc) {
   s <- 1.4826 * median(abs(x))
   converged <- FALSE
   k <- 0
-  while (!converged & k < 80) {
+  while (!converged && k < 80) {
     k <- k + 1
     v <- sqrt(2 * mean((2 * plogis(x / (s * 0.3739)) - 1) ^ 2))
     converged <- abs(v - 1) <= sqrt(.Machine$double.eps)
@@ -89,17 +91,18 @@ robScaleLocTest <- function(x, loc) {
 }
 
 # Test Known Location
-expect_equal(robScale(y, loc = 7), robScaleLocTest(y, loc = 7))
-expect_equal(robScale(1:3, loc = 3), robScaleLocTest(1:3, loc = 3))
+expect_equal(robScale(y, loc = 7), robScaleLocTest(y, loc = 7), tolerance = tol)
+expect_equal(robScale(1:3, loc = 3), robScaleLocTest(1:3, loc = 3),
+             tolerance = tol)
 expect_false(isTRUE(all.equal(robScale(1:3), robScaleLocTest(1:3, loc = 0))))
-expect_equal(robScale(1:3), mad(1:3))
+expect_equal(robScale(1:3), mad(1:3), tolerance = tol)
 
 # Test Error Trapping
 expect_error(robScale(c(x5, NA)), pattern = naErr)
-expect_equal(robScale(c(x5, NA), na.rm = TRUE), robScale(x5))
+expect_equal(robScale(c(x5, NA), na.rm = TRUE), robScale(x5), tolerance = tol)
 
 # Test Citation
-expect_true(any(grepl(packageVersion('revss'), toBibtex(citation('revss')),
+expect_true(any(grepl(packageVersion("revss"), toBibtex(citation("revss")),
                       fixed = TRUE)))
 
-message(paste("Seed for test session:", eff_seed))
+message("Seed for test session: ", eff_seed)
