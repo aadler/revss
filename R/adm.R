54d172b5d3c206fd863d8d585247c86381af433f
Avraham Adler <Avraham.Adler@gmail.com>
2026-01-06 19:54:22 -0500

    Create small-sample adm function and use factors from massive Monte Carlo test (may be redone) to update adm and madn.

--- R/adm.R
+++ R/adm.R
@@ -1,10 +1,65 @@
 # Copyright (c) 2020, Avraham Adler All rights reserved
 # SPDX-License-Identifier: BSD-2-Clause
 
-# Average Deviation from the Median with bias correction. Patterned after 'mad'
-# in stats
+# Mean Absolute Deviation from the Median (Average Deviation from the Mean) with
+# asymptotic bias correction. Patterned after 'mad' in stats
 
-adm <- function(x, center = median(x), constant = sqrt(pi / 2), na.rm = FALSE) {
+adm <- function(x, center = median(x), constant = NULL, na.rm = FALSE) {
   if (na.rm) x <- x[!is.na(x)]
+  n <- length(x)
+  if (n <= 1) {
+    stop("There needs to be at least two values for a robust median.")
+  }
+
+  # Asymptotic constant for both mean absolute deviation from the mean and mean
+  # absolute deviation from the median is sqrt(pi / 2)
+  if (is.null(constant)) {
+    constant <- 1.2533141373155001 # sqrt(pi / 2)
+  }
+
   constant * mean(abs(x - center))
+}
+
+# Mean Absolute Deviation from the Median (Average Deviation from the Mean) with
+# small-sample bias correction.
+
+admn <- function(x, center = c("median", "mean"), na.rm = FALSE) {
+
+  if (na.rm) x <- x[!is.na(x)]
+  n <- length(x)
+  if (n <= 1) {
+    stop("There needs to be at least two values for a robust measure.")
+  }
+  nc <- as.character(n)
+  center <- match.arg(center)
+  # Asymptotic constant for both mean absolute deviation from the mean and mean
+  # absolute deviation from the median is sqrt(pi / 2)
+  const <- 1.2533141373155001
+
+  if (center == "mean") {
+    an <- switch(nc,
+                 "2" = 1.414868530030482,
+                 "3" = 1.2248005760050789,
+                 "4" = 1.1547856730303148,
+                 "5" = 1.1172756685718443,
+                 "6" = 1.095847499979713,
+                 "7" = 1.0802842674190594,
+                 "8" = 1.068865412268323,
+                 "9" = 1.0606623968690487,
+                 n / (n - 0.51))
+  } else {
+    ne <- 2 * (n %/% 2) # Even floor of length. Constants exhibit step behavior.
+    an <- switch(nc,
+                 "2" = 1.414868530030482,
+                 "3" = 1.4143972249472192,
+                 "4" = 1.2031830896074811,
+                 "5" = 1.2023267518622953,
+                 "6" = 1.1345444408354397,
+                 "7" = 1.1343843879404827,
+                 "8" = 1.0999299985011299,
+                 "9" = 1.1000963990699943,
+                 ne / (ne - 0.76))
+  }
+
+  const * an * mean(abs(x - center))
 }
--- R/madn.R
+++ R/madn.R
@@ -1,30 +1,62 @@
 # Copyright (c) 2025, Avraham Adler All rights reserved
 # SPDX-License-Identifier: BSD-2-Clause
 
-# Median Absolute Deviation with small-sample bias correction based on Croux &
-# Rousseeuw (1992).
-# https://wis.kuleuven.be/stat/robust/papers/publications-1992/crouxrousseeuw-timeeffalgosnqn-compstat-1992.pdf # nolint line_length_linter
+# Median Absolute Deviation with small-sample bias correction. CR parameters
+# based on Croux & Rousseeuw (1992). AA parameters based on Monte Carlo by the
+# package owner, paper forthcoming.
+
 # Can replace 'mad' in stats for small samples.
 
-madn <- function(x, na.rm = FALSE) {
+madn <- function(x, center = c("median", "mean"), factors = c("AA", "CR"),
+                 na.rm = FALSE) {
+
   if (na.rm) x <- x[!is.na(x)]
   n <- length(x)
   if (n <= 1) {
-    stop("There needs to be at least two values for a robust median.")
+    stop("There needs to be at least two values for a robust measure.")
   }
-  # See the paper for the source of these values
-  bn <- switch(as.character(n),
-               "2" = 1.196,
-               "3" = 1.495,
-               "4" = 1.363,
-               "5" = 1.206,
-               "6" = 1.2,
-               "7" = 1.14,
-               "8" = 1.129,
-               "9" = 1.107,
-               n / (n - 0.8)
-  )
+  nc <- as.character(n)
+  no <- 2 * (n %/% 2) + 1L # Odd ceiling of length. Constants are step-like.
+  center <- match.arg(center)
+  factors <- match.arg(factors)
+  if (center == "mean") {
+    message("There are no factors in Croux & Rousseeuw for median absolute ",
+            "deviation from the mean. Using Adler's factors.")
+    factors <- "AA"
+    bn <- switch(nc,
+                 "2" = 1.196057420160531,
+                 "3" = 0.9430952345145196,
+                 "4" = 1.0171093862026159,
+                 "5" = 1.074608827423805,
+                 "6" = 1.0315309939442419,
+                 "7" = 1.0215452611271028,
+                 "8" = 1.0194719125368632,
+                 "9" = 1.0247557327860479,
+                 no / (no - 0.19))
+    return(bn * mad(x, center = mean(x)))
+  }
 
+  bn <- switch(factors,
+               CR = switch(nc,
+                           "2" = 1.196,
+                           "3" = 1.495,
+                           "4" = 1.363,
+                           "5" = 1.206,
+                           "6" = 1.2,
+                           "7" = 1.14,
+                           "8" = 1.129,
+                           "9" = 1.107,
+                           n / (n - 0.8)),
+               AA = switch(as.character(n),
+                           "2" = 1.196057420160531,
+                           "3" = 1.4882318165882795,
+                           "4" = 1.3605298448399872,
+                           "5" = 1.2158502434732232,
+                           "6" = 1.1899706508997985,
+                           "7" = 1.1388453803307483,
+                           "8" = 1.1273180435759429,
+                           "9" = 1.1010833093017414,
+                           n / (n - 0.8)))
   bn * mad(x)
 
 }
