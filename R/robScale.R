d16801462076984ee530358933f3dba023dbdaf2
Avraham Adler <Avraham.Adler@gmail.com>
2026-01-11 16:50:01 -0500

    clean robScale R code and reorder for efficiency

--- R/robScale.R
+++ R/robScale.R
@@ -6,18 +6,6 @@
 robScale <- function(x, loc = NULL, implbound = 1e-4, na.rm = FALSE,
                      maxit = 80L, tol = NULL, factors = c("AA", "CR")) {
 
-  if (is.null(tol)) {
-    tol <- sqrt(.Machine$double.eps)
-  } else {
-    tol <- as.double(tol)
-  }
-
-  if (missing(factors)) {
-    factors <- "AA"
-  } else {
-    factors <- match.arg(factors)
-  }
-
   if (!is.numeric(x)) {
     stop("x contains non-numeric entries.")
   }
@@ -30,14 +18,26 @@ robScale <- function(x, loc = NULL, implbound = 1e-4, na.rm = FALSE,
 
   x <- as.double(x)
 
+  if (is.null(tol)) {
+    tol <- sqrt(.Machine$double.eps)
+  } else {
+    tol <- as.double(tol)
+  }
+
+  if (missing(factors)) {
+    factors <- "AA"
+  } else {
+    factors <- match.arg(factors)
+  }
+
   if (!is.null(loc)) {
     x <- x - loc
     s <- 1.4826 * medianR(abs(x)) # MDZ in paper. Use 4 digits like mad.
-    t <- 0         # nolint object_overwrite_linter
+    t <- 0                        # nolint object_overwrite_linter
     minobs <- 3L
   } else {
     s <- madn(x, factors = factors)
-    t <- medianR(x) # nolint object_overwrite_linter
+    t <- medianR(x)              # nolint object_overwrite_linter
     minobs <- 4L
   }
 
@@ -49,5 +49,5 @@ robScale <- function(x, loc = NULL, implbound = 1e-4, na.rm = FALSE,
     }
   }
 
-  .Call(robScale_c, x, as.double(t), as.double(s), as.integer(maxit), tol)
+  .Call(robScale_c, x, t, as.double(s), as.integer(maxit), tol)
 }
